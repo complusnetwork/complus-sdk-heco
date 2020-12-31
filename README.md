791ce29117e7b393003d6af9da5cee36d50dc254
cryptosudo <cryptosudo@gmail.com>
2020-12-31 17:37:55 +0300

    Complus | SDK for HECO

---
+++ README.md
@@ -0,0 +1,23 @@
+# Complus | SDK FOR HECO
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![Actions Status](https://github.com/complusnetwork/complus-sdk-heco/workflows/CI/badge.svg)](https://github.com/complusnetwork/complus-sdk-heco)
+[![npm version](https://img.shields.io/npm/v/@complus/sdk-heco/latest.svg)](https://www.npmjs.com/package/@complus/sdk-heco/v/latest)
+[![npm bundle size (scoped version)](https://img.shields.io/bundlephobia/minzip/@complus/sdk-heco/latest.svg)](https://bundlephobia.com/result?p=@complus/sdk-heco@latest)
+
+## Huobi ECO Chain
+
+- [HECO Source Codes](https://github.com/HuobiGroup/huobi-eco-chain)
+- [HECO Explorer](https://scan.hecochain.com/)
+
+## Local Development
+
+The following assumes the use of `node@>=10`
+
+#### Install Dependencies
+
+`yarn`
+
+#### Run Build
+
+`yarn build`
---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 128,
+  TESTNET = 256
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xc32CCcf795940cA8491cD4F31161509DB28Ab719'
+
+export const INIT_CODE_HASH = '0x60e3516f18fde9a9c4ebc26c58261547ac802f880fe552105fb55bddcf7021c4'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
